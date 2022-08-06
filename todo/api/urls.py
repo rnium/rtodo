5edf7721d877c723ca153d8d474ea9c483899a5d
rony <rniumo@gmail.com>
2022-08-06 20:15:18 +0600

    8.06.22

--- todo/api/urls.py
+++ todo/api/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('active/', views.ActiveTodoView.as_view(), name='active'),
     path('completed/', views.CompletedTodoView.as_view(), name='complete'),
     path('trash/', views.TrashedTodoView.as_view(), name='trash'),
+    path('create/', views.create_todo, name='create'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
--- todo/api/views.py
+++ todo/api/views.py
@@ -1,9 +1,10 @@
 from rest_framework.generics import ListCreateAPIView, ListAPIView
 from rest_framework.response import Response
-from rest_framework
+from rest_framework.decorators import api_view
 from todo.models import Todo
 from .serializers import TodoSerializer, TrashedTodoSerializer
 from .pagination import TodoPagination
+from rest_framework import status
 
 
 class ActiveTodoView(ListCreateAPIView):
@@ -27,3 +28,18 @@ class TrashedTodoView(ListAPIView):
     def get_queryset(self):
         todos = Todo.objects.filter(user=self.request.user, trashed=True).order_by('-added')
         return todos
+
+@api_view
+def create_todo(request):
+    if request.method != 'POST':
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    kwargs = {}
+    kwargs['title'] = request.POST.get("title")
+    kwargs['user'] = request.user
+    description = request.POST.get("description", False)
+    if bool(description):
+        kwargs['description'] = description
+    important = request.POST.get("important", False)
+    if bool(important):
+        kwargs['important'] = True
+    return Response(kwargs)
